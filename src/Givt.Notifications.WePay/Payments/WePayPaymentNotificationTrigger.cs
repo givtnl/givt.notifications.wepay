c062fb91120640f56bcc49706a2104760ffade1e
Bjorn Derudder <bjorn.derudder@gmail.com>
2022-02-16 08:17:04 +0100

    Make sure we trace the transaction statuses in our database and log them when the status is not processed

--- src/Givt.Notifications.WePay/Payments/WePayPaymentNotificationTrigger.cs
+++ src/Givt.Notifications.WePay/Payments/WePayPaymentNotificationTrigger.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Threading.Tasks;
+using Givt.Business.Donations.Commands.UpdateTransactionStatusCommand;
 using Givt.Business.Infrastructure.Interfaces;
+using Givt.Models;
 using Givt.Notifications.WePay.Models;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Azure.Functions.Worker;
@@ -13,8 +16,11 @@ using Serilog.Sinks.Http.Logger;
 namespace Givt.Notifications.WePay.Payments;
 public class WePayPaymentNotificationTrigger: WePayNotificationTrigger
 {
-    public WePayPaymentNotificationTrigger(ISlackLoggerFactory loggerFactory, ILog logger) : base(loggerFactory, logger)
+    private readonly IMediator _mediator;
+
+    public WePayPaymentNotificationTrigger(ISlackLoggerFactory loggerFactory, ILog logger, IMediator mediator) : base(loggerFactory, logger)
     {
+        _mediator = mediator;
     }
 
     [Function("WePayPaymentNotificationTrigger")]
@@ -23,11 +29,42 @@ public class WePayPaymentNotificationTrigger: WePayNotificationTrigger
         var bodyString = await req.ReadAsStringAsync();
 
         var notification = JsonConvert.DeserializeObject<WePayNotification<WePayPayment>>(bodyString);
-        var logMessage = $"Payment with id {notification.Payload.Id} from {notification.Payload.CreationTime} has been updated to {notification.Payload.Status}";
         
+        var payment = notification.Payload;
+
+        await _mediator.Send(new UpdateTransactionStatusCommand
+        {
+            PaymentProviderId = payment.Id.ToString(),
+            NewTransactionStatus = convertWepayStatusToTransactionStatus(payment)
+        });
+
+        if (convertWepayStatusToTransactionStatus(payment) == TransactionStatus.Processed) return new OkResult();
+        
+        var logMessage = $"Payment with id {notification.Payload.Id} from {notification.Payload.CreationTime} has been updated to {notification.Payload.Status}";
         SlackLogger.Information(logMessage);
         Logger.Information(logMessage);
 
         return new OkResult();
+    }
+
+
+    private TransactionStatus convertWepayStatusToTransactionStatus(WePayPayment payment)
+    {
+        var status = TransactionStatus.Entered;
+        switch (payment.Status)
+        {
+            case "completed":
+                status = TransactionStatus.Processed;
+                break;
+            case "failed":
+            case "cancelled":
+                status = TransactionStatus.Cancelled;
+                break;
+            default:
+                status = TransactionStatus.All;
+                break;
+        }
+
+        return status;
     }
 }
