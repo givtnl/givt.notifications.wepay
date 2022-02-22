e64d781d1264483d32aceab1a06fc08a276d0d35
Bjorn Derudder <bjorn.derudder@gmail.com>
2022-02-22 11:28:35 +0100

    disputes resolved now also logs to slack

---
+++ src/Givt.Notifications.WePay/Payments/WePayPaymentDisputeResolvedNotificationTrigger.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Givt.Business.Infrastructure.Interfaces;
+using Givt.DatabaseAccess;
+using Givt.Notifications.WePay.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Serilog.Sinks.Http.Logger;
+using WePay.Clear.Generated.Model;
+
+namespace Givt.Notifications.WePay.Payments;
+
+public class WePayPaymentDisputeResolvedNotificationTrigger : WePayNotificationTrigger
+{
+    private readonly GivtDatabaseContext _context;
+
+    public WePayPaymentDisputeResolvedNotificationTrigger(ISlackLoggerFactory loggerFactory, ILog logger,
+        WePayNotificationConfiguration notificationConfiguration, GivtDatabaseContext context) : base(loggerFactory,
+        logger, notificationConfiguration)
+    {
+        _context = context;
+    }
+
+    [Function("WePayPaymentDisputeResolvedNotificationTrigger")]
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req)
+    {
+        var notification =
+            JsonConvert.DeserializeObject<WePayNotification<ResultDisputesResponse>>(await req.ReadAsStringAsync() ??
+                string.Empty);
+
+        var donations = await _context.Transactions.Where(x => x.PaymentProviderId == notification.Payload.Payment.Id)
+            .ToListAsync();
+
+        if (donations.Any())
+        {
+            var message = $"A dispute has been resolved for transaction with id {notification.Payload.Payment.Id}";
+            SlackLogger.Information(message);
+            Logger.Information(message);
+        }
+        else
+        {
+            var message =
+                $"A dispute has been resolved for transaction with id {notification.Payload.Payment.Id} but no donations were found in our system";
+            SlackLogger.Error(message);
+            Logger.Information(message);
+        }
+
+        return new OkResult();
+
+    }
+}
