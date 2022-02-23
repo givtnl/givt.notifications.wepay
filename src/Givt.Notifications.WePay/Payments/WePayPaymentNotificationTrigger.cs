ad2882ae3800e2146e190c2c2e9d9b2dc5e310c3
Maarten Vergouwe <maarten@givtapp.net>
2022-02-23 19:59:45 +0100

    Use ExceptionHandler

--- src/Givt.Notifications.WePay/Payments/WePayPaymentNotificationTrigger.cs
+++ src/Givt.Notifications.WePay/Payments/WePayPaymentNotificationTrigger.cs
@@ -25,27 +25,25 @@ public class WePayPaymentNotificationTrigger: WePayNotificationTrigger
     [Function("WePayPaymentNotificationTrigger")]
     public async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequestData req)
     {
-        var bodyString = await req.ReadAsStringAsync();
-
-        var notification = JsonSerializer.Deserialize<WePayNotification<WePayPayment>>(bodyString);
-        
-        var payment = notification.Payload;
-
-        await _mediator.Send(new UpdateTransactionStatusCommand
+        return await WithExceptionHandler(async Task<IActionResult>() => 
         {
-            PaymentProviderId = payment.Id.ToString(),
-            NewTransactionStatus = payment.TransactionStatus
+            var notification = await WePayNotification<WePayPayment>.FromHttpRequestData(req);
+            
+            var payment = notification.Payload;
+
+            await _mediator.Send(new UpdateTransactionStatusCommand
+            {
+                PaymentProviderId = payment.Id.ToString(),
+                NewTransactionStatus = payment.TransactionStatus
+            });
+
+            if (payment.TransactionStatus == TransactionStatus.Processed) return new OkResult();
+            
+            var logMessage = $"Payment with id {notification.Payload.Id} from {notification.Payload.CreationTime} has been updated to {notification.Payload.Status}";
+            SlackLogger.Information(logMessage);
+            Logger.Information(logMessage);
+
+            return new OkResult();
         });
-
-        if (payment.TransactionStatus == TransactionStatus.Processed) return new OkResult();
-        
-        var logMessage = $"Payment with id {notification.Payload.Id} from {notification.Payload.CreationTime} has been updated to {notification.Payload.Status}";
-        SlackLogger.Information(logMessage);
-        Logger.Information(logMessage);
-
-        return new OkResult();
-    }
-
-
-    
+    }    
 }
