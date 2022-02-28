c7d756e21cd75e6073f2e5d7500168a4c4e21b80
Bjorn Derudder <bjorn.derudder@gmail.com>
2022-02-28 14:23:42 +0100

    when a donation is done send the payment done mail to user

--- src/Givt.Notifications.WePay/Payments/WePayPaymentNotificationTrigger.cs
+++ src/Givt.Notifications.WePay/Payments/WePayPaymentNotificationTrigger.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Givt.Business.Donations.Commands.UpdateTransactionStatusCommand;
 using Givt.Business.Infrastructure.Interfaces;
@@ -10,6 +12,12 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Serilog.Sinks.Http.Logger;
 using System.Text.Json;
+using Givt.Business.Donations.Models;
+using Givt.Business.Donations.Queries;
+using Givt.Business.Payments.Commands.SendPaymentMail;
+using Givt.Business.Transactions.Queries;
+using Givt.Business.Users.Queries.GetDetail;
+using Givt.Models.Enums;
 
 [assembly: FunctionsStartup(typeof(Givt.Notifications.WePay.Startup))]
 namespace Givt.Notifications.WePay.Payments;
@@ -31,13 +39,45 @@ public class WePayPaymentNotificationTrigger: WePayNotificationTrigger
             
             var payment = notification.Payload;
 
-            await _mediator.Send(new UpdateTransactionStatusCommand
+            var updateResponse = await _mediator.Send(new UpdateTransactionStatusCommand
             {
                 PaymentProviderId = payment.Id.ToString(),
                 NewTransactionStatus = payment.TransactionStatus
             });
 
-            if (payment.TransactionStatus == TransactionStatus.Processed) return new OkResult();
+            if (payment.TransactionStatus == TransactionStatus.Processed)
+            {
+                if (!updateResponse.TransactionIds.Any() || !updateResponse.HasUserTransactions) return new OkResult();
+                
+                var transactions = await _mediator.Send(new GetDonationDetailQuery
+                {
+                    TransactionIds = new List<string> {payment.Id.ToString()}
+                });
+                
+                var user = await _mediator.Send(new GetUserDetailQuery
+                {
+                    Id = updateResponse.UserId
+                });
+                
+                // When the transaction is processed 
+                var sendPaymentCreatedMailCommand = new SendPaymentCreatedMailCommand()
+                {
+                    PaymentType = PaymentType.CreditCard,
+                    FirstName = user.FirstName,
+                    EmailAddress = user.Email,
+                    Amount = transactions.Sum(x => x.Amount),
+                    Language = "en",
+                    GiftOverview = transactions.Select(x => new AdvancedNoticePaymentListItem
+                    {
+                        TransactionId = x.Id,
+                        CollectGroupName = x.OrgName,
+                        Amount = x.Amount,
+                        DateTime = x.Timestamp
+                    }).ToList()
+                };
+                await _mediator.Send(sendPaymentCreatedMailCommand);
+                return new OkResult();
+            }
             
             var logMessage = $"Payment with id {notification.Payload.Id} from {notification.Payload.CreationTime} has been updated to {notification.Payload.Status}";
             SlackLogger.Information(logMessage);
